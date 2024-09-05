fd3313d9a8e2d5e8fcaac7bbd94aa7b1461d3920
Adam Wolfe Gordon <awg@upbound.io>
2024-09-05 16:20:10 -0600

    dependency: Separate cache operations from the add command
    Previously, `up dependency add` called without an argument would update the
    package cache, and called with `-c` would clean the cache. Neither of those
    operations really makes sense in `add`, so move them to their own
    commands (`update-cache` and `clean-cache`).
    
    While we're here, update the wording in the help to be project-centric and add
    `dep` as an alias so I can stop making typos in `dependency`.
    
    Signed-off-by: Adam Wolfe Gordon <awg@upbound.io>

--- cmd/up/dependency/add.go
+++ cmd/up/dependency/add.go
@@ -16,12 +16,10 @@ package dependency
 
 import (
 	"context"
-	"fmt"
 	"os"
 
 	"github.com/alecthomas/kong"
 	"github.com/crossplane/crossplane-runtime/pkg/errors"
-	"github.com/crossplane/crossplane/apis/pkg/v1beta1"
 	"github.com/pterm/pterm"
 	"github.com/spf13/afero"
 
@@ -33,9 +31,18 @@ import (
 	"github.com/upbound/up/internal/xpkg/workspace"
 )
 
-const (
-	errMetaFileNotFound = "crossplane.yaml file not found in current directory"
-)
+// addCmd manages crossplane dependencies.
+type addCmd struct {
+	m  *manager.Manager
+	ws *workspace.Workspace
+
+	Package string `arg:"" help:"Package to be added."`
+
+	// TODO(@tnthornton) remove cacheDir flag. Having a user supplied flag
+	// can result in broken behavior between xpls and dep. CacheDir should
+	// only be supplied by the Config.
+	CacheDir string `short:"d" help:"Directory used for caching package images." default:"~/.up/cache/" env:"CACHE_DIR" type:"path"`
+}
 
 // AfterApply constructs and binds Upbound-specific context to any subcommands
 // that have Run() methods that receive it.
@@ -44,47 +51,41 @@ func (c *addCmd) AfterApply(kongCtx *kong.Context, p pterm.TextPrinter) error {
 	ctx := context.Background()
 	fs := afero.NewOsFs()
 
-	cache, err := cache.NewLocal(c.CacheDir)
+	cache, err := cache.NewLocal(c.CacheDir, cache.WithFS(fs))
 	if err != nil {
 		return err
 	}
 
-	c.c = cache
+	r := image.NewResolver()
 
-	// only parse the workspace if we aren't attempting to clean the cache
-	if !c.CleanCache {
+	m, err := manager.New(
+		manager.WithCache(cache),
+		manager.WithResolver(r),
+	)
 
-		r := image.NewResolver()
+	if err != nil {
+		return err
+	}
 
-		m, err := manager.New(
-			manager.WithCache(cache),
-			manager.WithResolver(r),
-		)
+	c.m = m
 
-		if err != nil {
-			return err
-		}
+	wd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
 
-		c.m = m
+	ws, err := workspace.New(wd,
+		workspace.WithFS(fs),
+		workspace.WithPrinter(p),
+		workspace.WithPermissiveParser(),
+	)
+	if err != nil {
+		return err
+	}
+	c.ws = ws
 
-		wd, err := os.Getwd()
-		if err != nil {
-			return err
-		}
-
-		ws, err := workspace.New(wd,
-			workspace.WithFS(fs),
-			workspace.WithPrinter(p),
-			workspace.WithPermissiveParser(),
-		)
-		if err != nil {
-			return err
-		}
-		c.ws = ws
-
-		if err := ws.Parse(ctx); err != nil {
-			return err
-		}
+	if err := ws.Parse(ctx); err != nil {
+		return err
 	}
 
 	// workaround interfaces not being bindable ref: https://github.com/alecthomas/kong/issues/48
@@ -92,67 +93,8 @@ func (c *addCmd) AfterApply(kongCtx *kong.Context, p pterm.TextPrinter) error {
 	return nil
 }
 
-// addCmd manages crossplane dependencies.
-type addCmd struct {
-	c  *cache.Local
-	m  *manager.Manager
-	ws *workspace.Workspace
-
-	// TODO(@tnthornton) remove cacheDir flag. Having a user supplied flag
-	// can result in broken behavior between xpls and dep. CacheDir should
-	// only be supplied by the Config.
-	CacheDir   string `short:"d" help:"Directory used for caching package images." default:"~/.up/cache/" env:"CACHE_DIR" type:"path"`
-	CleanCache bool   `short:"c" help:"Clean dep cache."`
-
-	Package string `arg:"" optional:"" help:"Package to be added."`
-}
-
 // Run executes the dep command.
 func (c *addCmd) Run(ctx context.Context, p pterm.TextPrinter, pb *pterm.BulletListPrinter) error {
-	// no need to do anything else if clean cache was called.
-
-	// TODO (@tnthornton) this feels a little out of place here. We should
-	// consider adding a separate command for doing this.
-	if c.CleanCache {
-		if err := c.c.Clean(); err != nil {
-			return err
-		}
-		p.Printfln("xpkg cache cleaned")
-		return nil
-	}
-
-	if c.Package != "" {
-		if err := c.userSuppliedDep(ctx); err != nil {
-			return err
-		}
-		p.Printfln("%s added to xpkg cache", c.Package)
-		return nil
-	}
-
-	deps, err := c.metaSuppliedDeps(ctx)
-	if err != nil {
-		return err
-	}
-	if len(deps) == 0 {
-		p.Printfln("No dependencies specified")
-		return nil
-	}
-	p.Printfln("Dependencies added to xpkg cache:")
-	li := make([]pterm.BulletListItem, len(deps))
-	for i, d := range deps {
-		li[i] = pterm.BulletListItem{
-			Level:  0,
-			Text:   fmt.Sprintf("%s (%s)", d.Package, d.Constraints),
-			Bullet: "-",
-		}
-	}
-	// TODO(hasheddan): bullet list printer incorrectly appends an extra
-	// trailing newline. Update when fixed upstream.
-	return pb.WithItems(li).Render()
-}
-
-func (c *addCmd) userSuppliedDep(ctx context.Context) error {
-	// exit early check if we were supplied an invalid package string
 	_, err := xpkg.ValidDep(c.Package)
 	if err != nil {
 		return err
@@ -168,7 +110,7 @@ func (c *addCmd) userSuppliedDep(ctx context.Context) error {
 	meta := c.ws.View().Meta()
 
 	if meta != nil {
-		// crossplane.yaml file exists in the workspace, upsert the new dependency
+		// Metadata file exists in the workspace, upsert the new dependency
 		if err := meta.Upsert(ud); err != nil {
 			return err
 		}
@@ -178,29 +120,6 @@ func (c *addCmd) userSuppliedDep(ctx context.Context) error {
 		}
 	}
 
+	p.Printfln("%s added to xpkg cache", c.Package)
 	return nil
-}
-
-func (c *addCmd) metaSuppliedDeps(ctx context.Context) ([]v1beta1.Dependency, error) {
-	meta := c.ws.View().Meta()
-
-	if meta == nil {
-		return nil, errors.New(errMetaFileNotFound)
-	}
-
-	deps, err := meta.DependsOn()
-	if err != nil {
-		return nil, err
-	}
-
-	resolvedDeps := make([]v1beta1.Dependency, len(deps))
-	for i, d := range deps {
-		ud, _, err := c.m.AddAll(ctx, d)
-		if err != nil {
-			return nil, err
-		}
-		resolvedDeps[i] = ud
-	}
-
-	return resolvedDeps, nil
 }
