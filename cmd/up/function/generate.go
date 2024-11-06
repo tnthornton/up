db5286a7888928adf6be9360059b3a3dd67cc4d5
Christopher Haar <christopher.haar@upbound.io>
2024-11-06 12:54:52 +0100

    feat(fn-py): add default comment out in main.py (#230)
    * feat(fn-py): add default comment out in main.py
    
    Signed-off-by: Christopher Haar <christopher.haar@upbound.io>
    
    * feat(fn-py): add default comment out in main.py
    
    Signed-off-by: Christopher Haar <christopher.haar@upbound.io>
    
    ---------
    
    Signed-off-by: Christopher Haar <christopher.haar@upbound.io>

--- cmd/up/function/generate.go
+++ cmd/up/function/generate.go
@@ -117,6 +117,24 @@ from crossplane.function.proto.v1 import run_function_pb2 as fnv1
 
 def compose(req: fnv1.RunFunctionRequest, rsp: fnv1.RunFunctionResponse):
     pass
+    # Example to retrieve variables from "xr"; update as needed
+    # observed_xr = v1alpha1.XBucket(**req.observed.composite.resource)
+    # region = "us-west-1"
+    # if observed_xr.spec.region is not None:
+    #     region = observed_xr.spec.region
+
+    # Example S3 Bucket managed resource configuration; update as needed
+    # bucket = v1beta1.Bucket(
+    #     apiVersion="s3.aws.upbound.io/v1beta1",
+    #     kind="Bucket",
+    #     spec=v1beta1.Spec(
+    #         forProvider=v1beta1.ForProvider(
+    #             region=region,
+    #         ),
+    #     ),
+    # )
+
+    # resource.update(rsp.desired.resources["bucket"], bucket)
 `
 
 type kclModInfo struct {
